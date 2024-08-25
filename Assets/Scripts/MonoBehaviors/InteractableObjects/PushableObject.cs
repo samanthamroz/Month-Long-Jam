33b2d3b95c13217bce2671d164dd29099ff1ee8d
ktghostie <ktdied@gmail.com>
2024-08-25 10:12:53 -0400

    Updated PushableObject

--- Assets/Scripts/MonoBehaviors/InteractableObjects/PushableObject.cs
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/PushableObject.cs
@@ -13,24 +13,24 @@ public class PushableObject : InteractableObject
     public override void Interaction(GameObject player)
     {
         //check for left
-        if (player.transform.position.x < 0)
+        if (player.transform.position.x < gameObject.transform.position.x)
         {
             gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, 0, 0);
         }
         //check for right
-        else if (player.transform.position.x > 0)
+        else if (player.transform.position.x > gameObject.transform.position.x)
         {
             gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, 0, 0);
         }
         //check for top
-        else if (player.transform.position.y > 0)
-        {
-            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y + 1, 0);
-        }
-        //check for bottom
-        else if (player.transform.position.y < 0)
+        else if (player.transform.position.y > gameObject.transform.position.y)
         {
             gameObject.transform.position = new Vector3(0, gameObject.transform.position.y - 1, 0);
+        }
+        //check for bottom
+        else if (player.transform.position.y < gameObject.transform.position.y)
+        {
+            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y + 1, 0);
         }
     }
 
