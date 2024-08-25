76ebe626765336bc9cfc11481e92dee514b7b05d
ktghostie <ktdied@gmail.com>
2024-08-25 10:01:56 -0400

    Added MoveableBlock

---
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/PushableObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableObject : InteractableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void Interaction(GameObject player)
+    {
+        //check for left
+        if (player.transform.position.x < 0)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, 0, 0);
+        }
+        //check for right
+        else if (player.transform.position.x > 0)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, 0, 0);
+        }
+        //check for top
+        else if (player.transform.position.y > 0)
+        {
+            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y + 1, 0);
+        }
+        //check for bottom
+        else if (player.transform.position.y < 0)
+        {
+            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y - 1, 0);
+        }
+    }
+
+}
