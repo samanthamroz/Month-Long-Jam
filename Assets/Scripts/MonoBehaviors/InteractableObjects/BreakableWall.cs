b4cde4cfcf9851cf48e5e25eacea1f26be0888d7
samanthamroz <mrozsamantha@gmail.com>
2024-08-25 15:20:53 -0400

    Added boulder breaking wall

--- Assets/Scripts/MonoBehaviors/InteractableObjects/BreakableWall.cs
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/BreakableWall.cs
@@ -5,4 +5,12 @@ public class BreakableWall : InteractableObject
     public override void Interaction(GameObject player) {
         Destroy(gameObject);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("coll");
+        if (other.gameObject.tag == "Breaker") {
+            Destroy(gameObject);
+        }
+    }
 }
