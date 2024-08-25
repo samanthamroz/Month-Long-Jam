216da1747db08c9d85e6daa094ece6d626c53a13
samanthamroz <mrozsamantha@gmail.com>
2024-08-25 10:31:45 -0400

    Fixed moveable block

--- Assets/Scripts/MonoBehaviors/InteractableObjects/PushableObject.cs
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/PushableObject.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PushableObject : InteractableObject
 {
+    public int hitboxYOffset = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,26 +14,28 @@ public class PushableObject : InteractableObject
 
     public override void Interaction(GameObject player)
     {
-        //check for left
-        if (player.transform.position.x < gameObject.transform.position.x)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, 0, 0);
-        }
-        //check for right
-        else if (player.transform.position.x > gameObject.transform.position.x)
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, 0, 0);
-        }
-        //check for top
-        else if (player.transform.position.y > gameObject.transform.position.y)
-        {
-            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y - 1, 0);
-        }
-        //check for bottom
-        else if (player.transform.position.y < gameObject.transform.position.y)
-        {
-            gameObject.transform.position = new Vector3(0, gameObject.transform.position.y + 1, 0);
+        float horizontalDifference = gameObject.transform.position.x - player.transform.position.x;
+        float verticalDifference = gameObject.transform.position.y - (player.transform.position.y + hitboxYOffset);
+
+        //Debug.Log("Horizontal: " + horizontalDifference + ", Vertical: " + verticalDifference);
+
+        if (math.abs(horizontalDifference) > math.abs(verticalDifference)) {
+            if (horizontalDifference < 0) //check for left
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, 0);
+            } else { //check for right
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y, 0);
+            }
+        } else {
+            if (verticalDifference < 0) //check for top
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 1, 0);
+            }
+            //check for bottom
+            else
+            {
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0);
+            }
         }
     }
-
 }
--- Assets/Scripts/MonoBehaviors/InteractableObjects/RotatingPushableObject.cs
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/RotatingPushableObject.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class RotatingPushableObject : InteractableObject
 {
-    private Vector3 grabPointTop = new Vector3(0, 1, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +12,10 @@ public class RotatingPushableObject : InteractableObject
     }
 
     public override void Interaction(GameObject player) {
-        if (player.transform.position.x < grabPointTop.x) { //player on left
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, gameObject.transform.rotation.z + 45);
-        } else if (player.transform.position.x > grabPointTop.x) { //player on right
+        if (player.transform.position.x < gameObject.transform.position.x) { //player on left
             gameObject.transform.rotation = Quaternion.Euler(0, 0, gameObject.transform.rotation.z - 45);
+        } else if (player.transform.position.x > gameObject.transform.position.x) { //player on right
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, gameObject.transform.rotation.z + 45);
         }
     }
 }
