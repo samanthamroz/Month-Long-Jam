452ba9e83d45dbeb054bf86236415b833bdbb3bb
samanthamroz <mrozsamantha@gmail.com>
2024-09-03 18:48:44 -0400

    Added LeanTween and new camera/mvmt system

--- Assets/Scripts/MonoBehaviors/PlayerController.cs
+++ Assets/Scripts/MonoBehaviors/PlayerController.cs
@@ -4,22 +4,41 @@ using UnityEngine.InputSystem;
 //test
 public class PlayerController : MonoBehaviour
 {
+    private Camera cam;
     private InteractableObject currentInteraction;
     private Vector2 movementInput;
-    public float speed;
-    public bool canMove = true; //used to momentarily stop movement during minor events
+    public float movementSpeed;
+    public float cameraDrag;
+    public float characterDrag;
+    private bool canMove = true; //used to momentarily stop movement during minor events
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     void FixedUpdate()
     {
         if (canMove && movementInput != Vector2.zero) {
-            gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.transform.position + (new Vector3(movementInput.x, movementInput.y, 0) * speed * Time.deltaTime));
+            LeanTween.cancelAll();
+
+            //move character
+            //gameObject.GetComponent<Rigidbody2D>().MovePosition(gameObject.transform.position + (new Vector3(movementInput.x, movementInput.y, 0) * movementSpeed * Time.deltaTime));
+
+            LeanTween.value(gameObject, moveCharacter, gameObject.transform.position, gameObject.transform.position + (new Vector3(movementInput.x, movementInput.y, 0) * movementSpeed * Time.deltaTime), characterDrag);
+
+
+            //move camera
+            LeanTween.moveLocal(cam.gameObject, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10), cameraDrag).setEaseInBounce().setEaseOutSine();
         }
+        
+        
+    }
+
+    void moveCharacter(Vector3 input) {
+        Debug.Log(input);
+        gameObject.GetComponent<Rigidbody2D>().MovePosition(input);
     }
 
     private void OnMove(InputValue value) {
