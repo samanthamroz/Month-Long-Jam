91552cf2b99165a5bc17b993eb9e587e0b87aee0
ktghostie <ktdied@gmail.com>
2024-09-05 23:54:56 -0400

    Started on Destroying Extra Clones in Spawner

--- Assets/Scripts/MonoBehaviors/ObjectSpawner.cs
+++ Assets/Scripts/MonoBehaviors/ObjectSpawner.cs
@@ -8,6 +8,9 @@ public class ObjectSpawner : MonoBehaviour
     public GameObject prefab;
     public PlayerController playerController;
     private GameObject clone;
+    public int maxClones = 3;
+    private List<GameObject> clones = new List<GameObject>();
+    private GameObject currentClone;
     
     // Start is called before the first frame update
     void Start()
@@ -27,10 +30,22 @@ public class ObjectSpawner : MonoBehaviour
     private void SpawnBoulder() {
         clone = Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
         clone.GetComponent<RollableObject>().playerController = this.playerController;
+        clones.Add(currentClone);
 
         Vector3 originalScale = clone.transform.GetChild(0).localScale;
         clone.transform.GetChild(0).localScale = new Vector3(originalScale.x + .5f, originalScale.y + .5f, originalScale.z + .5f);
         LeanTween.scale(clone.transform.GetChild(0).gameObject, originalScale, spawnTime);
         //StartCoroutine(playerController.DoCutscene(spawnTime));
     }
+
+    void DestroyExtraClones()
+    {
+        while (clones.Count > maxClones)
+        {
+            GameObject cloneToDestroy = clones[0];
+            clones.RemoveAt(0);
+            Destroy(cloneToDestroy);
+        }
+    }
+
 }
