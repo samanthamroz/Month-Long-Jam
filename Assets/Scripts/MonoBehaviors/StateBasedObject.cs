b7dc00a90ad310f14b19ed2d3d8a44336539f771
samanthamroz <mrozsamantha@gmail.com>
2024-09-12 16:57:58 -0400

    Removed state-based objects

--- Assets/Scripts/MonoBehaviors/StateBasedObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class StateBasedObject : InteractableObject
-{
-    public abstract override void Interaction(GameObject player);
-    public Transform saveTransform {get {return gameObject.transform;} }
-}
