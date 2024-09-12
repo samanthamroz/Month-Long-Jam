d20bceaaa36b186a4e24d3f19406d548535d1b2c
samanthamroz <mrozsamantha@gmail.com>
2024-09-12 16:57:02 -0400

    added hint to water

--- Assets/Scripts/MonoBehaviors/SafeJump.cs
+++ Assets/Scripts/MonoBehaviors/SafeJump.cs
@@ -16,6 +16,15 @@ public class SafeJump : MonoBehaviour
     {
         if (collision.CompareTag("Water")) 
         {
+            /*
+            bool safelyTeleported = false
+            while (!safelyTeleported) {
+                try {
+                    Teleport() //this will try a teleport and throw an error if it doesn't work (i.e., if there are still contact points on the water)
+                    safelyTeleported = true
+                } catch {}
+            }
+            */
             TeleportOutsideTrigger(collision);
         }
     }
