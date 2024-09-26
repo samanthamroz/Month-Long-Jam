51bc60556f704a3e8cecf867fdb3bec15e48966e
ktghostie <ktdied@gmail.com>
2024-09-25 21:33:56 -0400

    Inventory UI Finished (I'm pretty sure)

--- Assets/Scripts/MonoBehaviors/UI/UIController.cs
+++ Assets/Scripts/MonoBehaviors/UI/UIController.cs
@@ -31,15 +31,28 @@ public class UIController : MonoBehaviour
     {
         GameObject pause = canvasRef.transform.GetChild(1).gameObject;
         pause.SetActive(isActive);
-        
         UpdateItemsList();
     }
 
     public void UpdateItemsList() {
         //Debug.Log(saveData.ingredientsCollected.Contains(Ingredient.BAKINGPOWDER));
-        GameObject mixer = canvasRef.transform.GetChild(1).GetChild(0).gameObject;
-        bool haveMixer = saveData.ingredientsCollected.Contains(Ingredient.BAKINGPOWDER);
-        mixer.SetActive(haveMixer);
+        GameObject bakingPowder = canvasRef.transform.GetChild(1).GetChild(0).gameObject;
+        bakingPowder.SetActive(saveData.ingredientsCollected.Contains(Ingredient.BAKINGPOWDER));
+        GameObject butter = canvasRef.transform.GetChild(1).GetChild(1).gameObject;
+        butter.SetActive(saveData.ingredientsCollected.Contains(Ingredient.BUTTER));
+        GameObject eggs = canvasRef.transform.GetChild(1).GetChild(2).gameObject;
+        eggs.SetActive(saveData.ingredientsCollected.Contains(Ingredient.EGGS));
+        GameObject flour = canvasRef.transform.GetChild(1).GetChild(3).gameObject;
+        flour.SetActive(saveData.ingredientsCollected.Contains(Ingredient.FLOUR));
+        GameObject milk = canvasRef.transform.GetChild(1).GetChild(4).gameObject;
+        milk.SetActive(saveData.ingredientsCollected.Contains(Ingredient.MILK));
+        GameObject sugar = canvasRef.transform.GetChild(1).GetChild(5).gameObject;
+        sugar.SetActive(saveData.ingredientsCollected.Contains(Ingredient.SUGAR));
+
+        GameObject mixer = canvasRef.transform.GetChild(1).GetChild(6).gameObject;
+        mixer.SetActive(saveData.itemsCollected.Contains(Tool.MIXER));
+        GameObject spatula = canvasRef.transform.GetChild(1).GetChild(7).gameObject;
+        mixer.SetActive(saveData.itemsCollected.Contains(Tool.SPATULA));
     }
 
     public void SetNotEnoughItemsPopupActive(bool isActive) {
