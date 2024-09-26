441e28225cdbcc3aeb53e1be5e706ac3a050db41
samanthamroz <mrozsamantha@gmail.com>
2024-09-26 18:28:28 -0400

    Renamed WaterRoom1.cs

---
+++ Assets/Scripts/MonoBehaviors/InteractableObjects/ToggleableObjects/WaterWheelRoom1.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterWheelRoom1 : ToggleableObject
+{
+    private bool hasTurned;
+    public float animationTime;
+    public GameObject breakableWall, leftStill, rightStill, rightFlowing, pipeFlowing;
+
+    public override bool GetSaveState()
+    {
+        return hasTurned;
+    }
+
+    public override string HoverText()
+    {
+        if (hasTurned) {
+            return null;
+        }
+        return "Turn";
+    }
+
+    public override void Interaction(GameObject player)
+    {
+        if (hasTurned) {
+            return;
+        }
+        
+        LeanTween.rotateAround(gameObject, Vector3.back, 360, animationTime);
+
+        StartCoroutine(player.GetComponent<PlayerController>().DoCutscene(animationTime));
+        ActivateRoom();
+        hasTurned = true;
+    }
+
+    private void ActivateRoom() {
+        pipeFlowing.SetActive(true);
+        leftStill.SetActive(true);
+        LeanTween.moveY(leftStill, -1, animationTime);
+        if (breakableWall.activeSelf) {
+            rightStill.SetActive(true);
+            LeanTween.moveY(rightStill, -1, animationTime);
+        } else {
+            rightFlowing.SetActive(true);
+            LeanTween.moveY(rightFlowing, 0, animationTime);
+        }
+        
+    }
+
+    public override void LoadFromSavedState(bool savedState)
+    {
+        if (savedState) {
+            animationTime = 0f;
+            ActivateRoom();
+        }
+    }
+}
