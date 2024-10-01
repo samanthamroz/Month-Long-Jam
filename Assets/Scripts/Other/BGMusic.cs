c1f4fdbb70005386c186a15f33a459d5cb46cc20
ktghostie <ktdied@gmail.com>
2024-10-01 15:12:50 -0400

    (If starting from title screen) Music Works as intended!

--- Assets/Scripts/Other/BGMusic.cs
+++ Assets/Scripts/Other/BGMusic.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BGMusic : MonoBehaviour
 {
+    //this BGMusic is ONLY for the dungeon music! the scenes listed in update have their own audio sources
+    //also i dont like using so many if else statements in update since those get called like
+    //every frame right? if theres a better way let me know 
+    public static BGMusic instance;
+    public bool paused = false;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (instance != null) {
+            Destroy(gameObject);
+        }
+        else {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (SceneManager.GetActiveScene().name == "TitleScreen") {
+            BGMusic.instance.GetComponent<AudioSource>().Pause();
+            paused = true;
+        }
+        else if (SceneManager.GetActiveScene().name == "Prologue") {
+            BGMusic.instance.GetComponent<AudioSource>().Pause();
+            paused = true;
+        }
+        else if (SceneManager.GetActiveScene().name == "Ending") {
+            BGMusic.instance.GetComponent<AudioSource>().Pause();
+            paused = true;
+        }
+        else if (paused) {
+            BGMusic.instance.GetComponent<AudioSource>().UnPause();
+        }
     }
 }
